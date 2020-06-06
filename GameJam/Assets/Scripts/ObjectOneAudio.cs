116a50dc63da1b5884f1b96a65c2f42432d6f691
Chris-TopherW <christhewratt@gmail.com>
2020-06-06 15:56:37 +0200

    Audio move to string based system with param

--- GameJam/Assets/Scripts/ObjectOneAudio.cs
+++ GameJam/Assets/Scripts/ObjectOneAudio.cs
@@ -4,34 +4,37 @@ using UnityEngine;
 
 public class ObjectOneAudio : MonoBehaviour
 {
+	public int ExtNotesModulo = 16;
+
     void Start()
     {
-    	GetComponent<ChuckSubInstance>().RunCode( @"
-    		class Crusher extends Chugen
-			{
-				8 => int bitDepth;
+
+		GetComponent<ChuckSubInstance>().RunCode( string.Format( @"
+			class Crusher extends Chugen
+			{{
+				2 => int bitDepth;
 				Math.pow(2, bitDepth) => float bitMultiplier;
 
 				fun void SetBitDepth(int value)
-				{
+				{{
 					Math.pow(2, value) => bitMultiplier;
-				}
+				}}
 
 				fun float tick(float input)
-				{
+				{{
 					input * bitMultiplier => input;
 					input $ int => input;
 					input $ float => input;
 					input / bitMultiplier => input;
 					return input;
-				}
-			}
+				}}
+			}}
 
 			[72, 75, 0, 72, 75, 0, 72, 70, 68, 0, 70, 72, 0, 77, 0, 70] @=> int p0Ch0Pr0[];
 			[0, 75, 77, 0, 75, 77, 0, 77, 75, 77, 0, 75, 72, 0, 77, 0] @=> int p1Ch0Pr0[];
 
 			SinOsc s0 => ADSR env0 => Crusher crusher => dac;
-			SinOsc s1 => env0 => crusher;
+			SinOsc s1 => env0;
 
 			50::ms => dur decay;
 			90::ms => dur NoteLength;
@@ -41,14 +44,14 @@ public class ObjectOneAudio : MonoBehaviour
 			0.4 => s0.gain;
 			0.4 => s1.gain;
 
-			16 => int notesModulo;
+		 	while(true)
+		 	{{
+		 		{0} => int notesModulo;
 
-			while(true)
-			{
-				if(notesModulo > 16) 16 => notesModulo;
-
-			    for(0 => int i; i < notesModulo; i++)
-			    {
+		 		if(notesModulo > 16) 16 => notesModulo;
+		 		
+		 		for(0 => int i; i < notesModulo; i++)
+			    {{
 			    	if(p0Ch0Pr0[i] == 0.0) 0.0 => s0.gain;
 					else 0.4 => s0.gain;
 					if(p1Ch0Pr0[i] == 0.0) 0.0 => s1.gain;
@@ -61,9 +64,8 @@ public class ObjectOneAudio : MonoBehaviour
 					10::ms + decay => now;
 					env0.keyOff();
 					NoteLength - 10::ms - decay => now;
-			    }		
-				
-			}
-		" );
+				}}
+			}}
+		", ExtNotesModulo ) );
     }
 }
