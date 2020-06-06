bae531fd2fc36577c58e7217087421765fbb4fd8
Chris-TopherW <christhewratt@gmail.com>
2020-06-06 15:13:21 +0200

    Basic melody added

--- GameJam/Assets/Scripts/ObjectOneAudio.cs
+++ GameJam/Assets/Scripts/ObjectOneAudio.cs
@@ -27,13 +27,40 @@ public class ObjectOneAudio : MonoBehaviour
 				}
 			}
 
-			SinOsc s => Crusher crusher => dac;
-			440 => s.freq;
+			[72, 75, 0, 72, 75, 0, 72, 70, 68, 0, 70, 72, 0, 77, 0, 70] @=> int p0Ch0Pr0[];
+			[0, 75, 77, 0, 75, 77, 0, 77, 75, 77, 0, 75, 72, 0, 77, 0] @=> int p1Ch0Pr0[];
+
+			SinOsc s0 => ADSR env0 => Crusher crusher => dac;
+			Phasor s1 => env0 => crusher;
+
+			40::ms => dur decay;
+			90::ms => dur NoteLength;
+			env0.set(10::ms, decay, 0.0, 10::ms);
 			crusher.SetBitDepth(4);
+			0 => int increment;
+
+			0.4 => s0.gain;
+			0.4 => s1.gain;
 
 			while(true)
-			{
-				1::second => now;
+			{				
+				if(p0Ch0Pr0[increment] == 0.0) 0.0 => s0.gain;
+				else 0.4 => s0.gain;
+				if(p1Ch0Pr0[increment] == 0.0) 0.0 => s1.gain;
+				else 0.4 => s1.gain;
+
+				Math.mtof(p0Ch0Pr0[increment]) => s0.freq;
+				Math.mtof(p1Ch0Pr0[increment]) => s1.freq;
+
+				env0.keyOn();
+				10::ms + decay => now;
+				env0.keyOff();
+				NoteLength - 10::ms - decay => now;
+				increment++;
+				if(increment >= 16)
+				{
+					0 => increment;
+				}
 			}
 		");
     }
