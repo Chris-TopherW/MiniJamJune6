427056334758d5d4fa4b106b545c39709f674d8f
Chris-TopherW <christhewratt@gmail.com>
2020-06-06 13:59:50 +0200

    Added sine wave test

---
+++ GameJam/Assets/ObjectOneAudio.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectOneAudio : MonoBehaviour
+{
+    void Start()
+    {
+    	GetComponent<ChuckSubInstance>().RunCode(@"
+    		SinOsc s => dac;
+    		440 => s.freq;
+
+    		while(true)
+    		{
+    			1::second => now;
+    		}
+
+    	");
+    }
+}
